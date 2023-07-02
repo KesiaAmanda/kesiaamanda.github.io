6b2cbd50dd13b36178a45877faf1c5103507871f
Kesia Amanda <kesia.ladeia@tgid.com.br>
2023-07-02 02:40:09 -0300

    🚚 adiciona pasta docs

--- docs/js/script.js
+++ docs/js/script.js
@@ -41,7 +41,8 @@ $(document).ready(function () {
     });
 
     $('#desktop').click(function () {
-        $('#startMenu').slideUp(100);
+        swapButtons();
+        $('#startMenu').addClass('hidden');
         $('#btnStart').removeClass('startClick').addClass('startRest');
     })
 
