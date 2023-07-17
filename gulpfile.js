db3c3611331459bc84ddc5d5ae8d762c21cffe13
Kesia Amanda <kesia.ladeia@tgid.com.br>
2023-07-16 22:57:02 -0300

    ✨ adiciona scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('sass', function () {
-    return gulp.src('caminho/do/seu/arquivo.scss')
+    return gulp.src('./portfolio/css/style.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('caminho/do/diretorio/de/saida'));
+        .pipe(gulp.dest('./portfolio/css'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('caminho/do/seu/arquivo.scss', gulp.series('sass'));
+    gulp.watch('./portfolio/css/style.scss', gulp.series('sass'));
+    gulp.watch('./portfolio/css/core/**', gulp.series('sass'));
+    gulp.watch('./portfolio/css/fonts/**', gulp.series('sass'));
+    gulp.watch('./portfolio/css/layout/**', gulp.series('sass'));
+    gulp.watch('./portfolio/css/utils/**', gulp.series('sass'));
 });
