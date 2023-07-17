c1a26305629c99651dbf55a1afb40d91f700da0e
Kesia Amanda <kesia.ladeia@tgid.com.br>
2023-07-17 00:44:18 -0300

    🐛 renomeia diretorio

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('sass', function () {
-    return gulp.src('./portfolio/css/style.scss')
+    return gulp.src('./docs/css/style.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./portfolio/css'));
+        .pipe(gulp.dest('./docs/css'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./portfolio/css/style.scss', gulp.series('sass'));
-    gulp.watch('./portfolio/css/core/**', gulp.series('sass'));
-    gulp.watch('./portfolio/css/fonts/**', gulp.series('sass'));
-    gulp.watch('./portfolio/css/layout/**', gulp.series('sass'));
-    gulp.watch('./portfolio/css/utils/**', gulp.series('sass'));
+    gulp.watch('./docs/css/style.scss', gulp.series('sass'));
+    gulp.watch('./docs/css/core/**', gulp.series('sass'));
+    gulp.watch('./docs/css/fonts/**', gulp.series('sass'));
+    gulp.watch('./docs/css/layout/**', gulp.series('sass'));
+    gulp.watch('./docs/css/utils/**', gulp.series('sass'));
 });
